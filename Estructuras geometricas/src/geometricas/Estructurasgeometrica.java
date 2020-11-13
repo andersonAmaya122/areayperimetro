2742e4e809a32fc44f3741f67656e3ad518719bd
ander <ander@LAPTOP-N8B260P5>
2020-11-12 22:30:09 -0500

    Comic anderson Amaya

--- Estructuras geometricas/src/geometricas/Estructurasgeometrica.java
+++ Estructuras geometricas/src/geometricas/Estructurasgeometrica.java
@@ -8,12 +8,12 @@ public class Estructurasgeometrica {
 	public static void main(String[] args) {
 	
 		 //circulo 	 
-	 double Pi = 3.14, R = 0, A = 0,  P= 0;
-	 R = Double.parseDouble(JOptionPane.showInputDialog("Digite el valor del radio"));
+	 double Pi = 3.14, R = 0, A = 0,  P= 0; 
+	  Double.parseDouble(JOptionPane.showInputDialog("Digite el valor del radio"));
 	 A = (Pi)* R*R;
 	 System.out.println("El area del circulo es : " + A );
  	  
-     R = Double.parseDouble(JOptionPane.showInputDialog("Digite el valor del radio"));
+     R = Double.parseDouble(JOptionPane.showInputDialog("2Digite el valor del radio"));
 	 P =  (Pi*Pi)*R	;
 	 System.out.println("El perimetro  del circulo es : " + P );
      
@@ -29,7 +29,6 @@ public class Estructurasgeometrica {
 	  double Al=12,B=10,A1=12,A2=13,A3=13;
 	  Al=(B*Al)/2;
 	  System.out.println("El  area del trinagulo es  : " + Al );
-	  
 	  P= A1+A2+A3;
 	  System.out.println("El perimetro  del trinagulo es  : " + P );
 	  
@@ -40,8 +39,11 @@ public class Estructurasgeometrica {
 	  System.out.println("El area del rectangulo  es  : " + A );
 	  
 	  
-	  P = (Ba*2)+(Ha*2);
+	  P=(Ba*2)+(Ha*2);
 	  System.out.println("El perimetro  del rectangulo es  : " + P );
+	  
+	   
+	  
 	  
 	 	
 	}
